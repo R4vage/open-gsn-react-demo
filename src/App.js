0d266789774455d6407431abd4212707f7524863
Julian Marc <julianmarc@gmail.com>
2023-04-20 13:24:13 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useEffect } from 'react';
+import { init, mint, transfer, getPaymasterBalance } from './Web3Client';
+
+function App() {
+  useEffect(() => {
+    init();
+  }, []);
+
+  const handleMint = () => {
+    mint(100);
+  };
+
+  const handleTransfer = () => {
+    transfer(100);
+  }
+
+
+
+  return (
+    <div className='App'>
+      <button onClick={handleMint}>Mint Tokens</button>
+      <button onClick={handleTransfer}>Transfer 100 Tokens</button>
+      <button onClick={getPaymasterBalance}>Console Paymaster Balance</button>
+    </div>
+  );
+}
+
+export default App;
